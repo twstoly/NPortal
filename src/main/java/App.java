3d6dee943c32097dbaa952decaa7e2aed8196e6c
twstoly <twstoly@gmail.com>
2022-02-13 11:06:08 +0300

    initializing the dao files in app java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,83 @@
+import Exceptions.ApiExceptions;
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oDepartmentNewsDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUserDao;
+import models.Department;
+import models.DepartmentNews;
+import models.News;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-        
+        Sql2oDepartmentNewsDao departmentNewsDao;
+        Sql2oNewsDao newsDao;
+        Sql2oUserDao userDao;
+        Sql2oDepartmentDao departmentDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:postgresql://localhost:5432/call_api";
+        Sql2o sql2o = new Sql2o(connectionString, "postgres", "1111111");
+
+
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        departmentNewsDao = new Sql2oDepartmentNewsDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
