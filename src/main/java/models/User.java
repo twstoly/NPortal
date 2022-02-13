bd10bbf5956b2f26c8713519acd80f3461a6bff6
twstoly <twstoly@gmail.com>
2022-02-13 11:25:46 +0300

    add object to object relationship to class user equals and hashcode methods overrides

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -37,6 +37,22 @@ public class User {
         this.departmentId = departmentId;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                departmentId == user.departmentId &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, role, departmentId);
+    }
+
     public void setId(int id) {
         this.id = id;
     }
