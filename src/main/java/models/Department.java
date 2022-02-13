731d66604fb3a2630be87922010b478bd1a88524
twstoly <twstoly@gmail.com>
2022-02-13 11:33:58 +0300

    adds Department class Instance variables and constructor that will initialize the instance variables

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -4,6 +4,17 @@ import java.util.Objects;
 
 public class Department {
 
+    private String name;
+    private String description;
+    private int numberOfEmployees;
+    private int id;
+
+    public Department(String name, String description, int numberOfEmployees) {
+        this.name=name;
+        this.description=description;
+        this.numberOfEmployees=numberOfEmployees;
+    }
+
     public String getName() {
         return name;
     }
